95860ebfceaa49b89feafe1c9f3d74de006e695c
Bastian Goonewardena <bastian@goonewardena.com>
2021-08-26 02:03:16 +1000

    Add Week1 scripts to portfolio

--- week1/practice.sh
+++ week1/practice.sh
@@ -4,6 +4,7 @@ echo "$1 has been able to get $2% for their exam"
 echo "The second best is $3 with 83%"
 
 echo "Please enter a Name, number and a name to run the script succesfully" 
+#read -p "Please enter a Name, number and a name to run the script succesfully" 1 2   #FIX ME??
 
 exit 0
 
