a95dd38504faa8ed9a07d2612ec92530b125a62d
Bastian Goonewardena <bastian@goonewardena.com>
2021-08-25 16:38:53 +1000

    Week2 wrk complteated

---
+++ week2/foldermaker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash 
+#This script will ask for a name of the folder and will ask for an output for the new folder that a user would lke to create.       
+read -p "Type the name of the folder you would like to create : " folderName  
+mkdir "$folderName" 
+exit 0
+
+     
