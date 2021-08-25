6e6f057d865535dad889801257ddd5ebfc9119e4
Bastian Goonewardena <bastian@goonewardena.com>
2021-08-25 21:52:29 +1000

    week 2 final

---
+++ week2/setPassword.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo " "
+read -p "Type a folder name: " folderName
+read -sp "Please type a password: " secretPassword
+echo " "
+mkdir "$folderName"
+touch "$folderName/secret.txt"
+
+echo ""
+echo "Folder Name:  $folderName created"
+
+#encrypt the password
+echo "$secretPassword" | sha3sum>$folderName/secret.txt
+echo "Encrypted password has been added to $folderName/secret.txt"
+echo " "
+echo " "
+
+#Enter Option to viwe the password:
+read -p "If you like to view the created password, please press 1: " optionOne
+if [ "$optionOne" -eq 1 ]; then
+	echo "The saved password is: " 
+	cat $folderName/secret.txt
+		else
+		echo "Please, Enter a correct input, exiting the program."
+fi
+exit 0
