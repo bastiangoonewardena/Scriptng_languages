618437283ee328aad20c64b312ef548c0fcf93cd
Bastian Goonewardena <bastian@goonewardena.com>
2021-08-25 11:47:32 +1000

    Add all week3

---
+++ week1/practice.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "The winners in the project are $1"
+echo "$1 has been able to get $2% for their exam"
+echo "The second best is $3 with 83%"
+
+echo "Please enter a Name, number and a name to run the script succesfully" 
+
+exit 0
+
