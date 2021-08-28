67a7e18e9c774db8a61c850598f8d093a2e37aa3
Bastian Goonewardena <bastian@goonewardena.com>
2021-08-29 02:59:15 +1000

    Add week6 work to portfolio

---
+++ week6/pythonExample.py
@@ -0,0 +1,14 @@
+#! /usr/bin/python3
+
+print("Hello world")
+
+value = "20"
+print ("The value is ", value)
+
+
+Y = 20
+
+if ( Y < 10 ):
+    print("Y is very smaller than 10")
+else:
+    print("Y is bigger than 10")
