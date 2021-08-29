aeb8ddbce3639c90198c5e7564aa34729b92749f
Bastian Goonewardena <bastian@goonewardena.com>
2021-08-30 00:00:23 +1000

    Add Week 1 scripts to portfolio

--- week1/second.sh
+++ week1/second.sh
@@ -1,5 +1,9 @@
-#!/bin/bash
+#! /bin/bash
+
+#User to provide the value for $1 by typing it into the command line after the path to the script.
+#To run the code ./second.sh or bash second.sh
+
 echo "Hi there!"
-echo "It's good to see you $1!"
+echo "It's good to see you $1!"   #String value entered after the script. 
 exit 0
 
