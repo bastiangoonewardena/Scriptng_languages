d5bf5573cf53d6a821a1883ca176ad88ee4c7bbd
Bastian Goonewardena <bastian@goonewardena.com>
2021-08-30 00:03:36 +1000

    Add Week 4 scripts to portfolio

--- week4/ipAddressesOnly.sh
+++ week4/ipAddressesOnly.sh
@@ -4,6 +4,9 @@
 #script and uses sed to display only the lines with 'IP Address' on them.
 
 #Follwing command will do all that in one ine of code. 
+#ipInfor.sh script is being used and piped it wwith sed command. 
+# -n meaning to spress the output. 
+# p at the end of script - only the lines with 'IP Address'
 ./ipInfo.sh | sed -n '/IP Address/ p'
 
 
