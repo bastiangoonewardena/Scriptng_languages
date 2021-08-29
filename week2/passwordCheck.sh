be47c78a54a8ac852e0308f404113b6fc9e37a9e
Bastian Goonewardena <bastian@goonewardena.com>
2021-08-30 00:03:10 +1000

    Add Week 2 scripts to portfolio

--- week2/passwordCheck.sh
+++ week2/passwordCheck.sh
@@ -1,28 +1,24 @@
-#!/bin/bash
-#
-#Write a script named 'passwordCheck.sh' that accomplishes the following:
-#Ask the user to type a secret password (hide that input).
-#Check the user’s password against the hash stored in 'secret.txt'.
-#If the user’s password is correct, print “Access Granted” and quit with the exit code 0.
-#If the user’s password is incorrect print “Access Denied” and quit with the exit code 1.
-#
+#! /bin/bash
+
 
 #creating the file "secret.txt"             
 touch secret.txt
 
+#echo will print to print the menu. and the banner. 
 echo "<<Welcome to password checking system>>"
 echo " "
 echo "1 - Set the password for the system"
 echo "2 - Check access to the system"
 echo " "
 
+#User input will be read, and stored as $optionSelect.
 read -p "Please select either option 1 or 2: " optionSelect
-if [ "$optionSelect" -eq 1 ]; then
+if [ "$optionSelect" -eq 1 ]; then     #Stating the basic if statment, if option user entered is equal to 1, then do the following.
 	echo " "
 	read -sp "Enter a password: " userPassword
 	echo " "
 	#creating the file "secret.txt"             
-#	touch secret.txt
+#	touch secret.txt #FIXME - Changed the location of careating the file to the top of the script. 
 
 	#Password encryption usin sha3sum, and output the restup to secret.txt file.
 	#hash3='echo $userPassword | sha3sum'
@@ -31,21 +27,21 @@ if [ "$optionSelect" -eq 1 ]; then
 	#cat secret.txt (Code was used for deuggning)
 	echo " "
 
-
+    #else statment, if the option equals 2. 
 	elif [ "$optionSelect" -eq 2 ]; then
 	echo " "
 	read -sp "Please enter your password to gain access to the system" userPreviousPassword
 	echo " "
 	
 			#Check the file secret.txt
-			hash1="$(cat secret.txt)"
-			hash2="$(echo $userPreviousPassword | sha3sum)"
+			hash1="$(cat secret.txt)"    #hash1 - output of the file secret.tex
+			hash2="$(echo $userPreviousPassword | sha3sum)"    #hash2 - output of the string and putting it to the sha3sum hash value.
 
 			#FixMe Error (Code was used for debugging)
 			#echo $hash1
 			#echo $hash2
 
-			if [ "$hash1" =  "$hash2" ]; then
+			if [ "$hash1" =  "$hash2" ]; then    #Checking to see the 2 hasgh values are equal, if so what happens, and else is if not. 
 				echo "Password you entered is CORRECT"
 				echo "Access Granted"
 			    exit 0
@@ -57,6 +53,7 @@ if [ "$optionSelect" -eq 1 ]; then
 
 
 		else
+		#Final Banner at the end of the script. 
 		echo "User option you entered is invalid, please try again later."
 		echo "to try again type ./passwordCheck_2.3.sh"
 		echo "Good Bye"
